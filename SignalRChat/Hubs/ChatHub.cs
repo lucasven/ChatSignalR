3004aafd2c9bbcc9efcc0f8979e6ff597d7e0455
lucasven <lucas.venturella@hotmail.com>
2015-08-18 17:31:04 -0300

    Primeiro commit do teste do SignalR

---
+++ SignalRChat/Hubs/ChatHub.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+
+namespace SignalRChat.Hubs
+{
+    public class ChatHub : Hub
+    {
+        public void Hello()
+        {
+            //Clients.All.hello();
+        }
+
+        public void Send(string name, string message)
+        {
+            if (message.Contains("foda"))
+            {
+                Clients.Caller.addNewMessageToPage(name, "Não é para falar palavrão seu puto");
+                return;
+            }
+            // Call the addNewMessageToPage method to update clients.
+            Clients.All.addNewMessageToPage(name, message);
+        }
+
+        public void sendWelcome(string name)
+        {
+            Clients.Caller.addNewMessageToPage("Sistema", "Seja bem vindo ao melhor chat do mundo " + name);
+        }
+    }
+}
